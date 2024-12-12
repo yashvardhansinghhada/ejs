adc9e163cb53eaff10d412a1be77ba139f36123e
unknown <amanhada022004@gmail.com>
2024-12-12 12:38:15 +0530

    init

--- node_modules/random-forest-classifier/index.js
+++
@@ -1,5 +0,0 @@
-var RandomForestClassifier = require('./forest'),
-    DecisionTreeClassifier = require('./tree');
-
-module.exports.RandomForestClassifier = RandomForestClassifier;
-module.exports.DecisionTreeClassifier = DecisionTreeClassifier;
